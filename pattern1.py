5e75b3339401bf8c0ae3ded297c0f9809f32c124
Srikanth <121529745+SrikanthKummari@users.noreply.github.com>
2024-11-13 12:39:23 +0530

    Pattern1.py

---
+++ pattern1.py
@@ -0,0 +1,18 @@
+num = int(input("enter the number of rows\n"))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+
+'''
+#input
+enter the number of rows
+5
+
+#output
+1
+12
+123
+1234
+12345
+'''
