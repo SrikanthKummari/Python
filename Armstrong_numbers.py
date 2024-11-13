1bc012e7d6877055a5295b36dcfaf3544f04049b
Srikanth <121529745+SrikanthKummari@users.noreply.github.com>
2024-11-13 11:50:57 +0530

    Armstrong_numbers.py

--- Armstrong_numbers.py
+++ Armstrong_numbers.py
@@ -11,3 +11,26 @@ for i in range(num):
 
     if arm == i:
         print(i, "is Armstrong number")
+
+
+'''
+#input
+enter a num
+1000
+
+#output
+0 is Armstrong number
+1 is Armstrong number
+2 is Armstrong number
+3 is Armstrong number
+4 is Armstrong number
+5 is Armstrong number
+6 is Armstrong number
+7 is Armstrong number
+8 is Armstrong number
+9 is Armstrong number
+153 is Armstrong number
+370 is Armstrong number
+371 is Armstrong number
+407 is Armstrong number
+'''
